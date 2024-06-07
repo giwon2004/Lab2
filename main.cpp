a1efab6bf45127448c4a9b8b42c728cc23d8c97a
Giwon Song <66408173+giwon2004@users.noreply.github.com>
2024-06-07 17:02:59 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,19 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+   if (cnt == M) {
+      for(int v: vec) cout << v << " "; cout << endl;
+      return;
+   }
+   int last = 0;
+   if (last = cnt) {
+      last = vec[cnt - 1];
+   }
+   for(int i = last; i < N; i++) {
+      vec.push_back(i+1);
+      func(cnt+1);
+      vec.pop_back();
+   }
 }
 
 int main() {
